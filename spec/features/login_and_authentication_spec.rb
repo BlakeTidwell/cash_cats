ef7ecae01cac6b29c4b4dc2f89f26370eb8fe5fb
Blake Tidwell <batidwell@gmail.com>
2015-08-16 23:33:17 -0700

    Adds skeleton for feature specs

---
+++ spec/features/login_and_authentication_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.feature "Login And Authentication", type: :feature do
+  it 'can register for an account'
+
+  context 'after creating an account' do
+    it 'can log into my account'
+  end
+end
---
+++ spec/features/recording_munnies_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.feature "Recording Munnies", type: :feature do
+  context 'when logged in' do
+    it 'can register for an account'
+  end
+end
