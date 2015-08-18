5d8e729fc592ffac2e8b20d27bc12b563fc8095d
Blake Tidwell <batidwell@gmail.com>
2015-08-17 22:54:14 -0700

    Fix munny recording spec description

--- spec/features/recording_munnies_spec.rb
+++ spec/features/recording_munnies_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.feature "Recording Munnies", type: :feature do
   context 'when logged in' do
-    it 'can register for an account'
+    it 'can add munny to my total and show it off on my profile'
   end
 end
